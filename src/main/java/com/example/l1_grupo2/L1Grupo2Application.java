8dfbb85b459e68ff28112f02a1298c67f4bf4aea
joelsilvera <joel.silvera@pucp.edu.pe>
2022-09-06 19:55:03 -0500

    tituol

---
+++ src/main/java/com/example/l1_grupo2/L1Grupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.l1_grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1Grupo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1Grupo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/l1_grupo2/L1Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l1_grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1Grupo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
